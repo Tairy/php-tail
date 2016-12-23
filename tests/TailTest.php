b71b9655fcc2546f6de4240c4376e984c2fcdae8
Tairy <tairyguo@gmail.com>
2016-12-23 10:30:01 +0800

    fix bug!

--- tests/TailTest.php
+++ tests/TailTest.php
@@ -7,4 +7,8 @@ use Tail\Tail;
 
 $t = new Tail('/Users/tairy/Documents/working/SF_Ops/test.txt');
 //$t->tailf();
-echo $t->tail(4);
+//echo $t->tail(4);
+while (true) {
+//    echo $t->readByPos($lastpos);
+//    echo $lastpos;
+}
